7dcbcc3ea03347d75da589fce3d5c6d68bfdc79a
binhqd@tech.est-rouge.com <binhqd@tech.est-rouge.com>
2019-03-16 10:30:32 +0700

    fix(User): Test commit for student
    In this commit, I've done something special ....
    
    #1

--- server/models/user.js
+++ server/models/user.js
@@ -3,8 +3,11 @@
 const async = require('async');
 let config = require('../../server/config.json');
 let path = require('path');
-let randomstring = require('randomstring');
 
 module.exports = function(User) {
   const self = User;
 };
+
+module.exports = function(User) {
+  let a = 5;
+}
